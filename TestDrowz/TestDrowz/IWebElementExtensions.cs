6b7a822a4fab3c25949754e91151c9f5e6e4ef0e
Oleg <korol_oleg@mail.ru>
2014-11-25 12:09:29 +0200

    Еще один автотест

---
+++ TestDrowz/TestDrowz/IWebElementExtensions.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+
+namespace TestDrowz
+{
+    public static class IWebElementExtensions
+    {
+        public static string GetValue(this IWebElement element)
+        {
+            return element.GetAttribute("value");
+        }
+    }
+}
